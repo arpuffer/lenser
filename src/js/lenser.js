d773d20076ee1e9e1c0cb087cf56289f4b487b49
Alex Puffer <alex.puffer@gmail.com>
2021-03-21 23:57:56 -0700

    Replaced jpg with an SVG graphic for background

--- src/js/lenser.js
+++ src/js/lenser.js
@@ -1,7 +1,9 @@
 function focalLengthChanged() {
     const focalLength = document.getElementById("focal-len-slider").value;
     var focalLenDisp = document.getElementById("focal-len-display");
+    var lenserAppImg = document.getElementById("lenser-app-image");
     focalLenDisp.innerText = focalLength;
+    lenserAppImg.style.backgroundSize = focalLength + "%";
     console.log("Focal length changed to " + focalLength + "mm");
 }
 
